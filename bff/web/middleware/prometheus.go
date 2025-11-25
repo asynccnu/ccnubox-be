63bc4fe494a8802e99a94d605bbde8375a4fdc3d
agermel <agermel@mails.ccnu.edu.cn>
2025-11-25 09:11:00 +0800

    feat: remove student_id from prometheus for better aggregation

--- bff/web/middleware/prometheus.go
+++ bff/web/middleware/prometheus.go
@@ -1,12 +1,11 @@
 package middleware
 
 import (
-	"github.com/asynccnu/ccnubox-be/bff/pkg/ginx"
-	"github.com/asynccnu/ccnubox-be/bff/pkg/prometheusx"
-	"github.com/asynccnu/ccnubox-be/bff/web/ijwt"
-	"github.com/gin-gonic/gin"
 	"net/http"
 	"time"
+
+	"github.com/asynccnu/ccnubox-be/bff/pkg/prometheusx"
+	"github.com/gin-gonic/gin"
 )
 
 type PrometheusMiddleware struct {
@@ -24,21 +23,26 @@ func NewPrometheusMiddleware(
 func (m *PrometheusMiddleware) MiddlewareFunc() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		start := time.Now()
-		path := ctx.Request.URL.Path
+		// path := ctx.Request.URL.Path
+		path := ctx.FullPath()
+		if path == "" {
+			path = "not found"
+		}
+		m.prometheus.ActiveConnections.WithLabelValues(path).Inc()
 		defer func() {
-			m.prometheus.ActiveConnections.WithLabelValues(path).Inc()
+			// 由于学号数据量太过高基数且太过离散，这里的数据从个体请求数的采集改为总体请求数
 
 			// TODO 这里没有想到更加简单便捷的方案去判断是否需要记录学号,所以全都记录了
-			var studentId = "no studentId"
-			uc, _ := ginx.GetClaims[ijwt.UserClaims](ctx)
-			if uc.StudentId != "" {
-				studentId = uc.StudentId
-			}
+			// var studentId = "no studentId"
+			// uc, _ := ginx.GetClaims[ijwt.UserClaims](ctx)
+			// if uc.StudentId != "" {
+			// 	studentId = uc.StudentId
+			// }
 
 			// 记录响应信息
 			m.prometheus.ActiveConnections.WithLabelValues(path).Dec()
 			status := ctx.Writer.Status()
-			m.prometheus.RouterCounter.WithLabelValues(ctx.Request.Method, path, http.StatusText(status), studentId).Inc()
+			m.prometheus.RouterCounter.WithLabelValues(ctx.Request.Method, path, http.StatusText(status)).Inc()
 			m.prometheus.DurationTime.WithLabelValues(path, http.StatusText(status)).Observe(time.Since(start).Seconds())
 
 		}()

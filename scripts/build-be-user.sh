de17bf3012e020f4348b506f03345b0d5ed124b4
sunjunnan <2101763682@qq.com>
2025-12-18 14:30:45 +0800

    fix:修复秘钥问题

--- scripts/build-be-user.sh
+++ scripts/build-be-user.sh
@@ -9,12 +9,6 @@ CRYPTO_KEY=$2
 
 speciald="be-user"
 
-## 这里注意需要自己配置一个加密key，否则会使用默认的key，存在安全隐患
-## 本地调试可忽略
-if [[ -n "$CRYPTO_KEY" ]]; then
-  CRYPTO_KEY="muxiStudio123456"
-fi
-
 echo -e "🔧🔧🔧 Building and pushing image for $speciald 🔧🔧🔧 \n"
 
 docker build -t "$speciald:v1" -f "./$speciald/Dockerfile" --build-arg KEY="$CRYPTO_KEY" .

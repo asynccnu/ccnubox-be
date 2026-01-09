82ce3568afc9ae2cc677b90f8d9de453e26a9ef6
sunjunnan <2101763682@qq.com>
2026-01-09 15:28:01 +0800

    fix:修复feed参数校验

--- bff/web/feed/feed_vo.go
+++ bff/web/feed/feed_vo.go
@@ -5,28 +5,28 @@ type GetFeedEventsResp struct {
 }
 
 type FeedEvent struct {
-	Id           int64             `json:"id" binding:"required"`
-	Title        string            `json:"title" binding:"required"`
-	Type         string            `json:"type" binding:"required"`
-	Content      string            `json:"content" binding:"required"`
-	CreatedAt    int64             `json:"created_at" binding:"required"` //Unix时间戳
-	ExtendFields map[string]string `json:"extend_fields" binding:"required"`
+	Id           int64             `json:"id"`
+	Title        string            `json:"title"`
+	Type         string            `json:"type"`
+	Content      string            `json:"content"`
+	CreatedAt    int64             `json:"created_at"` //Unix时间戳
+	ExtendFields map[string]string `json:"extend_fields"`
 }
 type FeedEventVO struct {
-	Id           int64             `json:"id" binding:"required"`
-	Title        string            `json:"title" binding:"required"`
-	Type         string            `json:"type" binding:"required"`
-	Content      string            `json:"content" binding:"required"`
-	CreatedAt    int64             `json:"created_at" binding:"required"` //Unix时间戳
-	ExtendFields map[string]string `json:"extend_fields" binding:"required"`
-	Read         bool              `json:"read" binding:"required"`
+	Id           int64             `json:"id"`
+	Title        string            `json:"title"`
+	Type         string            `json:"type"`
+	Content      string            `json:"content"`
+	CreatedAt    int64             `json:"created_at"` //Unix时间戳
+	ExtendFields map[string]string `json:"extend_fields"`
+	Read         bool              `json:"read"`
 }
 type MuxiOfficialMSG struct {
-	Title        string            `json:"title" binding:"required"`
-	Content      string            `json:"content" binding:"required"`
-	ExtendFields map[string]string `json:"extend_fields" binding:"required"` //自定义拓展字段
-	PublicTime   int64             `json:"public_time" binding:"required"`   //发布的时间
-	Id           string            `json:"id" binding:"required"`
+	Title        string            `json:"title"`
+	Content      string            `json:"content"`
+	ExtendFields map[string]string `json:"extend_fields"` //自定义拓展字段
+	PublicTime   int64             `json:"public_time"`   //发布的时间
+	Id           string            `json:"id" `
 }
 
 type ClearFeedEventReq struct {
@@ -46,35 +46,33 @@ type ChangeFeedAllowListReq struct {
 }
 
 type GetFeedAllowListResp struct {
-	Grade   bool `json:"grade" binding:"required"`
-	Muxi    bool `json:"muxi" binding:"required"`
-	Holiday bool `json:"holiday" binding:"required"`
-	Energy  bool `json:"energy" binding:"required"`
-}
-type ChangeElectricityStandardReq struct {
-	ElectricityStandard bool `json:"electricity_standard" binding:"required"`
+	Grade   bool `json:"grade" `
+	Muxi    bool `json:"muxi" `
+	Holiday bool `json:"holiday" `
+	Energy  bool `json:"energy" `
 }
 
 type SaveFeedTokenReq struct {
 	Token string `json:"token" binding:"required"`
 }
+
 type RemoveFeedTokenReq struct {
 	Token string `json:"token" binding:"required"`
 }
 
 type PublicMuxiOfficialMSGReq struct {
-	Title        string            `json:"title" binding:"required"`
-	Content      string            `json:"content" binding:"required"`
-	ExtendFields map[string]string `json:"extend_fields" binding:"required"`
-	LaterTime    int64             `json:"later_time" binding:"required"` //延迟多久发布(单位是秒)
+	Title        string            `json:"title"`
+	Content      string            `json:"content"`
+	ExtendFields map[string]string `json:"extend_fields"`
+	LaterTime    int64             `json:"later_time"` //延迟多久发布(单位是秒)
 }
 
 type PublicMuxiOfficialMSGResp struct {
-	Title        string            `json:"title" binding:"required"`
-	Content      string            `json:"content" binding:"required"`
-	PublicTime   string            `json:"public_time" binding:"required"`
-	ExtendFields map[string]string `json:"extend_fields" binding:"required"`
-	Id           string            `json:"id" binding:"required"`
+	Title        string            `json:"title"`
+	Content      string            `json:"content"`
+	PublicTime   string            `json:"public_time"`
+	ExtendFields map[string]string `json:"extend_fields"`
+	Id           string            `json:"id"`
 }
 
 type StopMuxiOfficialMSGReq struct {
@@ -82,5 +80,5 @@ type StopMuxiOfficialMSGReq struct {
 }
 
 type GetToBePublicMuxiOfficialMSGResp struct {
-	MSGList []MuxiOfficialMSG `json:"msg_list" binding:"required"`
+	MSGList []MuxiOfficialMSG `json:"msg_list"`
 }

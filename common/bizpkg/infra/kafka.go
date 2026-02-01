fbc4a3fbf8c151b546581bbd3f45366ce3b23a40
weiji6 <2954585173@qq.com>
2026-02-01 21:41:55 +0800

    feat:新增kafka密码校验

--- common/bizpkg/infra/kafka.go
+++ common/bizpkg/infra/kafka.go
@@ -9,6 +9,12 @@ import (
 
 func InitKafka(cfg *conf.KafkaConf) sarama.Client {
 	saramaCfg := sarama.NewConfig()
+
+	saramaCfg.Net.SASL.Enable = true
+	saramaCfg.Net.SASL.User = cfg.Username
+	saramaCfg.Net.SASL.Password = cfg.Password
+	saramaCfg.Net.SASL.Mechanism = sarama.SASLTypePlaintext
+
 	saramaCfg.Producer.Return.Successes = true
 	saramaCfg.Producer.Partitioner = sarama.NewConsistentCRCHashPartitioner
 	client, err := sarama.NewClient(cfg.Addrs, saramaCfg)
